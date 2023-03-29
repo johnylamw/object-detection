7ae68feedbb7d923d36dbd33fcca8832dddc5801
johnylamw <johnylamw@gmail.com>
2023-03-28 20:58:20 -0700

    MainReactor sampler -- not tested

--- detection.cpp
+++ detection.cpp
@@ -1,26 +1,35 @@
 
 #include <opencv2/opencv.hpp>
+#include <shared_mat/shared_mat.h>
 #include <stdio.h>
 using namespace cv;
 using namespace std;
 int main(int argc, char** argv)
 {
-    Mat videoMat;
-    namedWindow("Video Display");
-
-    // Create video capture on camera port 0
-    VideoCapture cap(0);
-    if (!cap.isOpened()) {
-        cout << "Cannot open camera";
+    if (argc == 1 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+        // cout << "Usage: ./detection " << endl;
     }
 
-    // Display the video mat
-    while (true) {
-        cap >> videoMat;
-        imshow("Video Display", videoMat);
-        if ((char) 113 == (char) waitKey(1)) {
+    // MainReactor Setup:
+    string colorMatName = "DAI_COLOR_0";
+    string depthMatName = "DAI_DEPTH_0";
+    
+    SharedMat sharedColorMat(colorMatName.c_str());
+    SharedMat sharedDepthMat(depthMatName.c_str());
+
+    while (1) {
+        sharedColorMat.waitForFrame();
+        sharedDepthMat.waitForFrame();
+
+        Mat colorMat = sharedColorMat.mat;
+        Mat depthMat = sharedDepthMat.mat;
+
+        imshow("Color Mat", colorMat);
+        imshow("Depth Mat", depthMat);
+
+        if (waitKey(1) == 27) {
             break;
         }
     }
-    return 0;
+
 }
