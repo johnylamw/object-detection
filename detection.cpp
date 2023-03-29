88a3a4b21e61fca49a67e053fe8d04b1ff04e2f3
johnylamw <johnylamw@gmail.com>
2023-03-28 20:36:10 -0700

    sample video capturing

--- detection.cpp
+++ detection.cpp
@@ -1,5 +1,26 @@
-#include <iostream>
 
-int main(int, char**) {
-    std::cout << "XBOT 488\n";
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+using namespace cv;
+using namespace std;
+int main(int argc, char** argv)
+{
+    Mat videoMat;
+    namedWindow("Video Display");
+
+    // Create video capture on camera port 0
+    VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        cout << "Cannot open camera";
+    }
+
+    // Display the video mat
+    while (true) {
+        cap >> videoMat;
+        imshow("Video Display", videoMat);
+        if ((char) 113 == (char) waitKey(1)) {
+            break;
+        }
+    }
+    return 0;
 }
