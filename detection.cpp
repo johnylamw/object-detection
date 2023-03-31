2a86ed7e5fdcbc1434c92df182ed1a223d3493c2
johnylamw <johnylamw@gmail.com>
2023-03-30 19:38:46 -0700

    added blob detection setup

--- detection.cpp
+++ detection.cpp
@@ -4,8 +4,7 @@
 #include <stdio.h>
 using namespace cv;
 using namespace std;
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
     if (argc == 1 || strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
         // cout << "Usage: ./detection " << endl;
     }
@@ -17,6 +16,10 @@ int main(int argc, char** argv)
     SharedMat sharedColorMat(colorMatName.c_str());
     SharedMat sharedDepthMat(depthMatName.c_str());
 
+    // Set up Blob Detection
+    SimpleBlobDetector detector = *setupBlobDetection();
+    
+    // Looping through each iterations of frames
     while (1) {
         sharedColorMat.waitForFrame();
         sharedDepthMat.waitForFrame();
@@ -52,9 +55,48 @@ int main(int argc, char** argv)
         Mat morphologyFrame;
         cv::morphologyEx(thresholdFrame, morphologyFrame, cv::MORPH_ELLIPSE, kernel, cv::Point(-1, -1), 4, 0 ,cv::morphologyDefaultBorderValue());
 
+        // Detect Blobs
+        std::vector<KeyPoint> keypoints;
+        detector.detect(morphologyFrame, keypoints);
+        
+        Mat keypointsFrame;
+        cv::drawKeypoints(colorFrame, keypoints, keypointsFrame, cv::Scalar(0, 0, 255), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+        // im_with_keypoints = cv.drawKeypoints(color_frame, keypoints, np.array([]), (0,0,255), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
         if (waitKey(1) == 27) {
             break;
         }
     }
-
 }   
+
+Ptr<SimpleBlobDetector> setupBlobDetection() {
+    cv::SimpleBlobDetector::Params params;
+    
+    params.minDistBetweenBlobs = 20;
+    params.minThreshold = 0;
+    params.maxThreshold = 10;
+    params.thresholdStep = 10;
+    params.minRepeatability = 1;
+
+    params.filterByColor = true;
+    params.blobColor = 0;
+    
+    params.filterByArea = true;
+    params.minArea = 1000;
+    params.maxArea = 240000;
+
+    params.filterByCircularity = false;
+    // params.minCircularity = 0.1
+    // params.maxCircularity = 0.5 
+    
+    params.filterByInertia = false;
+    // params.minInertiaRatio = 0.01
+    // params.maxInertiaRatio = 0.80
+    
+    params.filterByConvexity = false;
+    
+    Ptr<SimpleBlobDetector> detector = cv::SimpleBlobDetector::create(params);
+    
+    return detector;
+}
